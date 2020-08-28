43be3d3f3a68a8e181ba592170310c3472371abf
Yevheniia Drozdova <evgesha.drozdova@gmail.com>
2020-08-28 15:29:48 +0300

    додала зум на 5 графік

--- js/draw__5.js
+++ js/draw__5.js
@@ -16,8 +16,9 @@ const svg_5 = pre_svg
     .append("g")
     .attr("transform", "translate(" + scatter_margin.left + "," + 0 + ")");
 
-var scatter_x1 = d3.scaleBand().padding([0.1]);
-var scatter_x2 = d3.scaleBand().padding([0.2]);
+
+var scatter_x1 = d3.scaleBand().padding([0.2]);
+var scatter_x2 = d3.scaleBand()
 
 var scatter_yScale = d3.scaleBand();
 
@@ -31,9 +32,17 @@ svg_5
     
 
 //Add group for the y axis
-svg_5.append("g")
+var scatter_yAxis = svg_5.append("g")
     .attr("class", "y-axis")
     .attr("transform", "translate(" + 0 + "," + 20 + ")");
+
+
+var clip = scatter_yAxis.append("rect")
+    .attr("x", -120)
+    .attr("y", 0)
+    .attr("fill", charts_background)
+
+;
 
 
 function draw_scatter(df){
@@ -42,6 +51,13 @@ function draw_scatter(df){
     var new_width = d3.select("#chart-block-4").select(".col-1-2").node().getBoundingClientRect().width - scatter_margin.left - scatter_margin.right;
     var new_height = df.y_domain.length * 17 + scatter_margin.top;
 
+
+    pre_svg.call(zoom);
+
+
+    clip
+        .attr("width", 120)
+        .attr("height", new_height);
 
 
 // Pan and zoom
@@ -71,17 +87,6 @@ function draw_scatter(df){
         // ])
         .domain([0, 1000000]);
 
-
-    svg_5.select(".y-axis")
-        .transition()
-        .duration(zero_duration)
-        .call(d3.axisLeft(scatter_yScale)
-            .tickSize(0)
-            .tickFormat(function(d) {
-                return d.substring(0, 12) + "...";
-            })
-            .tickSizeOuter(0)
-        );
 
     svg_5.select(".x-axis")
         .attr("transform", "translate(" + 0 + "," + new_height + ")")
@@ -132,8 +137,8 @@ function draw_scatter(df){
 
     circles.enter().append("circle")
         .attr("class", "circle tip")        
-        .style("fill", transparentBlue)
-        .style("stroke", "grey")
+        .style("fill", "#007EFF80")
+        .style("stroke", "#007EFF")
         .style("opacity", 1)
         .transition().duration(transition_time)
         .attr("cx", function (k) {
@@ -149,5 +154,56 @@ function draw_scatter(df){
 
 
     circles.exit().remove();
+
+
+    svg_5.select(".y-axis").raise()
+        .transition()
+        .duration(zero_duration)
+        .call(d3.axisLeft(scatter_yScale)
+            .tickSize(0)
+            .tickFormat(function(d) {
+                return d.substring(0, 12) + "...";
+            })
+            .tickSizeOuter(0)
+        );
+
+    function zoom() {
+        const extent = [[0, 0], [new_width, new_height]];
+
+        pre_svg.call(d3.zoom()
+            .scaleExtent([1, 8])
+            .translateExtent(extent)
+            .extent(extent)
+            .on("zoom", zoomed));
+
+        function zoomed() {
+            scatter_x1
+                .range([0, new_width]
+                .map(function(d) { return d3.event.transform.applyX(d) } ));
+
+            scatter_x2
+                .domain([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31])
+                .range([0, scatter_x1.bandwidth()])
+            ;
+
+            svg_5.select(".x-axis")
+                .attr("transform", "translate(" + 0 + "," + new_height + ")")
+                .transition()
+                .duration(transition_time)
+                .call(d3.axisBottom(scatter_x1)
+                    .ticks(3)
+                    .tickSizeOuter(0));
+
+            d3.select("#chart_5").selectAll(".group").attr("transform", function(d){
+                let key = parseInt(d.key);
+                return `translate(${scatter_x1(key)}, 30)`
+            });
+
+            d3.select("#chart_5").selectAll(".circle")
+                .attr("cx", function (k) {
+                    return scatter_x2(k.day)}
+                )
+        }
+    }
 
 }
